254157f218a45fbc539748879edcb94bdeb281ae
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2023-01-23 20:46:50 -0700

    update

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,3 +1,32 @@
+// import React, { Component } from 'react';
+
+// class NumberOfEvents extends Component {
+//   state = { num: 32 };
+
+//   changeNum(value) {
+//     this.setState({ num: value });
+//   }
+
+//   render() {
+//     const { num } = this.state;
+
+//     return (
+//       <input
+//         className='num'
+//         type='number'
+//         value={num}
+//         onChange={(event) => {
+//           this.changeNum(event.target.value);
+//         }}
+//       >
+//       </input>
+//     );
+//   }
+// }
+
+// export default NumberOfEvents;
+
+
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
