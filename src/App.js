addd2301b1c02d7754a40e3552dc2e0bdc7c5ad2
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2023-02-12 10:33:54 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -24,34 +24,49 @@ class App extends Component {
 
 
 
+  // updateEvents = (location, inputNumber) => {
+  //   const {eventCount, seletedLocation} = this.state;
+  //   if (location) {
+  //     getEvents().then((events) => {
+  //       const locationEvents = (location === 'all') ?
+  //       events :
+  //       events.filter((event) => event.location === location);
+  //       const eventsToShow=locationEvents.slice(0, eventCount);
+  //       this.setState({
+  //       events: eventsToShow,
+  //       seletedLocation: location
+  //       });
+  //     });  
+  //   } else {
+  //     getEvents().then((events) => {
+  //       const locationEvents = (seletedLocation === 'all') ?
+  //       events :
+  //       events.filter((event) => event.location === seletedLocation);
+  //       const eventsToShow=locationEvents.slice(0, inputNumber);
+  //       this.setState({
+  //         events: eventsToShow,
+  //         eventCount: inputNumber
+  //       });
+  //     })
+  //   }
+  // }
+
   updateEvents = (location, inputNumber) => {
-    const {eventCount, seletedLocation} = this.state;
-    if (location) {
-      getEvents().then((events) => {
-        const locationEvents = (location === 'all') ?
-        events :
-        events.filter((event) => event.location === location);
-        const eventsToShow=locationEvents.slice(0, eventCount);
-        this.setState({
-        events: eventsToShow,
-        seletedLocation: location
-        });
-      });  
-    } else {
-      getEvents().then((events) => {
-        const locationEvents = (seletedLocation === 'all') ?
-        events :
-        events.filter((event) => event.location === seletedLocation);
-        const eventsToShow=locationEvents.slice(0, inputNumber);
-        this.setState({
-          events: eventsToShow,
-          eventCount: inputNumber
-        });
-      })
-    }
-  }
-
-
+    const { eventCount } = this.state;
+    if (location === undefined) location = this.state.selectedLocation;
+    getEvents().then((events) => {
+      const locationEvents =
+        location === 'all'
+          ? events
+          : events.filter((event) => event.location === location);
+      inputNumber = inputNumber === undefined ? eventCount : inputNumber;
+      this.setState({
+        events: locationEvents.slice(0, inputNumber),
+        selectedLocation: location,
+        eventCount: inputNumber,
+      });
+    });
+  };
  
 
   async componentDidMount() {
@@ -88,27 +103,29 @@ class App extends Component {
     if (this.state.showWelcomeScreen === undefined) return <div
     className="App"></div>
     
-    if (navigator.onLine) {
-      console.log('online');
-    } else {
-      console.log('offline');
-    }
-    window.addEventListener('offline', (e) => { console.log('offline'); });
+  //   if (navigator.onLine) {
+  //     console.log('online');
+  //   } else {
+  //     console.log('offline');
+  //   }
+  //   window.addEventListener('offline', (e) => { console.log('offline'); });
 
-   window.addEventListener('online', (e) => { console.log('online'); });
+  //  window.addEventListener('online', (e) => { console.log('online'); });
     return (
       <div className="App">
         <div className="offline-alert">
           {!navigator.onLine && (
          <OfflineAlert 
           text= {'You are currently offline. The event list may not be up-to-date.'}
-          className='OfflineAlert'
+          // className='OfflineAlert'
          />)}
+
+         
         </div>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents updateEvents={this.updateEvents}/>
         <EventList events={this.state.events} />
-        <OfflineAlert/>
+        {/* <OfflineAlert/> */}
         <WelcomeScreen
           showWelcomeScreen={this.state.showWelcomeScreen}
           getAccessToken={() => {
