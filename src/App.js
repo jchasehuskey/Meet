f1bc4a070390c674b3eaf9086ea87f42fce9f800
jchasehuskey <101059291+jchasehuskey@users.noreply.github.com>
2023-02-12 14:43:37 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import NumberOfEvents from './NumberOfEvents';
 import WelcomeScreen from './WelcomeScreen';
 import { getEvents, extractLocations, checkToken, getAccessToken } from
 './api';
-// import { getEvents, extractLocations } from './api';
 import './nprogress.css';
 import {OfflineAlert} from './Alert';
 
@@ -22,75 +21,6 @@ class App extends Component {
     showWelcomeScreen: undefined
   }
 
-
-
-
-
-  // updateEvents = (location, inputNumber) => {
-  //   const { eventCount } = this.state;
-  //   if (location === undefined) location = this.state.selectedLocation;
-  //   if (navigator.onLine) {
-  //     getEvents().then((events) => {
-  //       const locationEvents =
-  //         location === 'all'
-  //           ? events
-  //           : events.filter((event) => event.location === location);
-  //       inputNumber = inputNumber === undefined ? eventCount : inputNumber;
-  //       this.setState({
-  //         events: locationEvents.slice(0, inputNumber),
-  //         selectedLocation: location,
-  //         eventCount: inputNumber,
-  //       });
-  //       localStorage.setItem('events', JSON.stringify(events));
-  //       console.log('events', localStorage.getItem('events'));
-  //       localStorage.setItem('locations', JSON.stringify(extractLocations(events)));
-  //       console.log('locations', localStorage.getItem('locations'));
-  //     });
-  //   } else {
-
-      
-  //     this.setState({
-  //       events: JSON.parse(localStorage.getItem('events')) || [],
-  //       locations: JSON.parse(localStorage.getItem('locations')) || [],
-  //       selectedLocation: location,
-  //       eventCount: inputNumber || eventCount,
-       
-  //     });
-  //     console.log('navigator is offline');
-  //     console.log('events from local storage', this.state.events);
-  //     console.log('locations from local storage', this.state.locations);
-  //   }
-  // };
-
-  // async componentDidMount() {
-  //   this.mounted = true;
-  //   const accessToken = localStorage.getItem('access_token');
-  //   const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-  //   const searchParams = new URLSearchParams(window.location.search);
-  //   const code = searchParams.get('code');
-  //   this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-  //   if ((code || isTokenValid) && this.mounted) {
-  //     if (navigator.onLine) {
-  //       getEvents().then((events) => {
-  //         if (this.mounted) {
-  //           this.setState({
-  //             events: events.slice(0, this.state.numberOfEvents),
-  //             locations: extractLocations(events),
-  //           });
-  //           localStorage.setItem('events', JSON.stringify(events));
-  //           console.log('events', localStorage.getItem('events'));
-  //           localStorage.setItem('locations', JSON.stringify(extractLocations(events)));
-  //           console.log('locations', localStorage.getItem('locations'));
-  //         }
-  //       });
-  //     } else {
-  //       this.setState({
-  //         events: JSON.parse(localStorage.getItem('events')) || [],
-  //         locations: JSON.parse(localStorage.getItem('locations')) || [],
-  //       });
-  //     }
-  //   }
-  // }
 
   updateEvents = (location, inputNumber) => {
     const {eventCount, seletedLocation} = this.state;
@@ -115,6 +45,7 @@ class App extends Component {
           events: eventsToShow,
           eventCount: inputNumber
         });
+        console.log([eventsToShow], [eventCount]);
       })
     }
   }
@@ -145,59 +76,6 @@ class App extends Component {
       });
     }
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // updateEvents = (location, inputNumber) => {
-  //   const { eventCount } = this.state;
-  //   if (location === undefined) location = this.state.selectedLocation;
-  //   getEvents().then((events) => {
-  //     const locationEvents =
-  //       location === 'all'
-  //         ? events
-  //         : events.filter((event) => event.location === location);
-  //     inputNumber = inputNumber === undefined ? eventCount : inputNumber;
-  //     this.setState({
-  //       events: locationEvents.slice(0, inputNumber),
-  //       selectedLocation: location,
-  //       eventCount: inputNumber,
-  //     });
-  //   });
-  // };
- 
-
-  // async componentDidMount() {
-  //   this.mounted = true;
-  //   const accessToken = localStorage.getItem('access_token');
-  //   const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-  //   const searchParams = new URLSearchParams(window.location.search);
-  //   const code = searchParams.get('code');
-  //   this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-  //   if ((code || isTokenValid) && this.mounted) {
-
-  //     getEvents().then((events) => {
-  //       if (this.mounted) {
-  //         this.setState({
-  //           events: events.slice(0, this.state.numberOfEvents),
-  //           locations: extractLocations(events)
-  //         });
-  //       }
-  //     });
-  //   }
-  // }
-
-
-
 
 
   componentWillUnmount(){
